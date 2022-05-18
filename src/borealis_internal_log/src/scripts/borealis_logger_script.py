9799262d18a49b508e8137052c8a1658ed618ab6
BorealisJames <james_halim@sutd.edu.sg>
2022-05-18 15:49:05 +0800

    logger script done

--- src/borealis_internal_log/src/scripts/borealis_logger_script.py
+++ src/borealis_internal_log/src/scripts/borealis_logger_script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
 
+from ast import arg
 import multiprocessing
 import signal
 import time
@@ -7,45 +8,29 @@ import os
 import subprocess
 from datetime import datetime
 from pythonping import ping
+import psutil
 
 def handler(signum, frame):
     print("Stopping logger")
-    # p1.kill()
+    p1.kill()
     p2.kill()
+    p3.kill()
+    p4.kill()
     exit()
-
-def slow_timer():
-    while True:
-        time.sleep(1)
-        print("Slow timer")
 
 def write_ping(ip_add, ping_file):
     output = ping(ip_add, verbose=False)
+    now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S.%f")[:-3] 
     f = open(ping_file, 'a')
     f.write(str(output))
     f.write("\n")
-    now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S") 
     f.write(now)
+    f.write(",")
     f.write("\n")
     f.write("\n")
     f.close()
 
-def read_wifi_strength():
-    p = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = p.stdout.read().decode()
-    p.communicate()
-
-    return out
-
-def write_wifi_strength(wifi_file):
-    output = read_wifi_strength()
-    f = open(wifi_file, 'a')
-    f.write(str(output))
-    f.write(now)
-    f.write("\n")
-    f.close()
-
-def ping_timer():
+def ping_timer(hz):
     global drone_number, path_to_store_logs
 
     if drone_number == "1":
@@ -58,31 +43,84 @@ def ping_timer():
     f.close()
 
     while True:
-        time.sleep(0.5)
+        time.sleep(hz)
         if drone_number == "1":
             write_ping('192.168.1.62',ping_file)
-
         if drone_number == "2":
             write_ping('192.168.1.63',ping_file)
 
-def wifi_timer():
+def read_wifi_strength():
+    p = subprocess.Popen("iwconfig", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.stdout.read().decode()
+    p.communicate()
+
+    return out
+
+def write_wifi_strength(wifi_file):
+    output = read_wifi_strength()
+    now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S.%f")[:-3] 
+    f = open(wifi_file, 'a')
+    f.write(str(output))
+    f.write(now)
+    f.write(",")
+    f.write("\n")
+    f.close()
+
+def wifi_timer(hz):
     global path_to_store_logs
 
     wifi_file = path_to_store_logs + "/wifi_signal_strength_log.txt"
-    f = open(wifi_file, 'w')
-    f.write("Starting wifi Logs")
+    while True:
+        time.sleep(hz)
+        write_wifi_strength(wifi_file)
+
+def write_cpu_strength(cpu_file, hz):
+    output = psutil.cpu_percent(hz) # avg cpu usage for 0.5 seconds
+    now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S.%f")[:-3] 
+    f = open(cpu_file, 'a')
+    f.write(str(output))
+    f.write("\n")
+    f.write(now)
+    f.write(",")
+    f.write("\n")
     f.close()
 
+def cpu_timer(hz):
+    global path_to_store_logs
+
+    cpu_file = path_to_store_logs + "/cpu_strength_log.txt"
+    while True:
+        write_cpu_strength(cpu_file, hz)
+
+def write_ram_usage(ram_file):
+    output = psutil.virtual_memory()[2] # ram usage 
+    now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S.%f")[:-3] 
+    f = open(ram_file, 'a')
+    f.write(str(output))
+    f.write("\n")
+    f.write(now)
+    f.write(",")
+    f.write("\n")
+    f.close()
+
+def ram_timer(hz):
+    global path_to_store_logs
+
+    ram_file = path_to_store_logs + "/ram_usage.txt"
     while True:
         time.sleep(0.5)
-        write_wifi_strength(wifi_file)
+        write_ram_usage(ram_file)
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, handler)
-    p1 = multiprocessing.Process(target=wifi_timer)
     drone_number = os.getenv('DRONE_NUMBER')
     print("Drone number is : {}".format(drone_number))
-    p2 = multiprocessing.Process(target=ping_timer)
+
+    p1 = multiprocessing.Process(target=wifi_timer,args=[0.5])
+    p2 = multiprocessing.Process(target=ping_timer,args=[0.5])
+    p3 = multiprocessing.Process(target=cpu_timer,args=[0.5]) # in percantage
+    p4 = multiprocessing.Process(target=ram_timer,args=[0.5]) # in percantage
 
     now = datetime.now().strftime("%d_%m_%Y_time_%H_%M_%S")
     path_to_store_logs = os.path.expanduser('~/Diagnosis/') + now
@@ -93,5 +131,7 @@ if __name__ == "__main__":
     f.write(now)
     f.close()
 
-    p2.start()
     p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
